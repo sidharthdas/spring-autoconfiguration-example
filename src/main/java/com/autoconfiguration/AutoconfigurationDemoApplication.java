fa8b3f106051afab8f4ff12c36a7ab424be70e18
sidharthdas <sidharthdas.1995@gmail.com>
2023-08-22 20:08:07 +0530

    Updated the context

--- src/main/java/com/autoconfiguration/AutoconfigurationDemoApplication.java
+++ src/main/java/com/autoconfiguration/AutoconfigurationDemoApplication.java
@@ -8,15 +8,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootApplication
 /*@ComponentScan(value = {"config"})*/
-public class AutoconfigurationDemoApplication {
+public class AutoconfigurationDemoApplication implements CommandLineRunner{
+
+    @Autowired
+    private WebApplicationContext context;
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(AutoconfigurationDemoApplication.class, args);
+        SpringApplication.run(AutoconfigurationDemoApplication.class, args);
+    }
+    @Override
+    public void run(String... args) throws Exception {
         AppConfig appConfig = context.getBean(AppConfig.class);
         System.out.println(appConfig.getName());
-
     }
 }
